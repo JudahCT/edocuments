db77ccbd620442b2acd2f5505a61093832275051
Sithabile <nyathiesithabilestar@gmail.com>
2021-02-26 10:06:05 +0200

    commit

---
+++ edocs/server/email.js
@@ -0,0 +1,41 @@
+const sgMail = require('@sendgrid/mail');
+
+
+
+sgMail.setApiKey("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");
+
+//ES6
+// sgMail
+//     .send(msg)
+//     .then(() => {}, error => {
+//         console.error(error);
+
+//         if (error.response) {
+//             console.error(error.response.body)
+//         }
+//     });
+//ES8
+let sendMail = async(email) => {
+
+    const msg = {
+        to: email,
+        from: 'snyathi@nbsz.co.zw', // Use the email address or domain you verified above
+        subject: 'Sending with Twilio SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even widddddddddddth Node.js</strong>',
+    };
+
+
+    try {
+        await sgMail.send(msg);
+        console.log("Email sent successfully .......")
+    } catch (error) {
+        console.error(error);
+
+        if (error.response) {
+            console.error(error.response.body)
+        }
+    }
+}
+
+export default sendMail
